fbf732c50e3eb3c1118354a849a371d069e487b9
thinkholic <mailtokasun@gmail.com>
2015-11-05 14:30:31 +0530

    Initial Commit

---
+++ libs/url-helper.js
@@ -0,0 +1,15 @@
+UrlHelper = {};
+UrlHelper.queryParams = {};
+
+UrlHelper.queryParams.getValue = function(q) {
+  q = q.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+  var regex = new RegExp("[\\?&]" + q + "=([^&#]*)"),
+      results = regex.exec(location.search);
+  return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+};
+
+if(typeof module !== "undefined") {
+  module.exports = UrlHelper;
+} else {
+  window.LoginState = UrlHelper;
+}
